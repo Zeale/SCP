66777fbdc5d243dff0c958cb566d01b1dbdd6292
Zeale <zeale@dusttoash.org>
2020-11-27 18:48:14 -0600

    Create User.java

---
+++ src/scp/main/serverconn/User.java
@@ -0,0 +1,30 @@
+package scp.main.serverconn;
+
+import java.net.Socket;
+
+/**
+ * Used by the server to keep track of users who connect in. Each {@link User}
+ * has a username and connection (the connection is a {@link Socket}) associated
+ * with them.
+ * 
+ * @author Gartham
+ *
+ */
+public class User {
+	private final Socket socket;
+	private final String username;
+
+	public Socket getSocket() {
+		return socket;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public User(Socket socket, String username) {
+		this.socket = socket;
+		this.username = username;
+	}
+
+}

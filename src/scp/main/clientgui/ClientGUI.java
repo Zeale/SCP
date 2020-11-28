53062aec1e872e1d5a4d2572096b0c73cf5598d1
Zeale <zeale@dusttoash.org>
2020-11-27 18:20:48 -0600

    Create ClientGUI.java

---
+++ src/scp/main/clientgui/ClientGUI.java
@@ -0,0 +1,32 @@
+package scp.main.clientgui;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.guis.windows.Window;
+
+public class ClientGUI extends Window {
+	
+	/*
+	 * private @FXML Node firstGUIElement;
+	 * private @FXML Button sendButton;
+	 * ...
+	 */
+	
+	private @FXML void initialize() {
+		// TODO perform GUI initialization using FXML nodes above.
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+
+	}
+
+}

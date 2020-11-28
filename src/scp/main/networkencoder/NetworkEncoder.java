8c98d7cf523de42698465700563c69e70ee068d7
Dillon Shomaker <43662297+DillonS91@users.noreply.github.com>
2020-11-27 20:17:45 -0600

    Create NetworkEncoder.java

---
+++ src/scp/main/networkencoder/NetworkEncoder.java
@@ -0,0 +1,23 @@
+package scp.main.networkencoder;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class NetworkEncoder {
+	public static byte[] encodeMessage(String message) {
+		byte[] msg = message.getBytes(StandardCharsets.UTF_8),
+				size = { (byte) ((msg.length & 0xFF000000) >>> 6), (byte) ((msg.length & 0xFF0000) >>> 4),
+						(byte) ((msg.length & 0xFF00) >>> 2), (byte) (msg.length & 0xFF) };
+		byte[] res = new byte[4 + msg.length];
+		System.arraycopy(size, 0, res, 0, 4);
+		System.arraycopy(msg, 0, res, 4, msg.length);
+		return res;
+	}
+
+	public static String pollMessage(InputStream input) throws IOException {
+		DataInputStream dis = new DataInputStream(input);
+		return new String(dis.readNBytes(dis.readInt()), StandardCharsets.UTF_8);
+	}
+}

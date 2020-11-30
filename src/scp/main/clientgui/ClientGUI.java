566c248222e6dda9f0ca71c3566f98330617ce0f
Gartham <gartham@dusttoash.org>
2020-11-29 22:57:20 -0600

    Add fxml props to ClientGUI

--- src/scp/main/clientgui/ClientGUI.java
+++ src/scp/main/clientgui/ClientGUI.java
@@ -1,18 +1,28 @@
 package scp.main.clientgui;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 public class ClientGUI extends Window {
-	
+
 	/*
-	 * private @FXML Node firstGUIElement;
-	 * private @FXML Button sendButton;
-	 * ...
+	 * private @FXML Node firstGUIElement; private @FXML Button sendButton; ...
 	 */
-	
+
+	private @FXML TextField serverIP, serverPort, username, localPort;
+
+	private @FXML void launchClient() {
+		// TODO Auto-generated method stub
+	}
+
+	private @FXML void launchServer() {
+		// TODO Auto-generated method stub
+	}
+
 	private @FXML void initialize() {
 		// TODO perform GUI initialization using FXML nodes above.
 	}
@@ -27,6 +37,10 @@ public class ClientGUI extends Window {
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
 		// TODO Auto-generated method stub
 
+	}
+
+	private @FXML void close() {
+		Platform.exit();
 	}
 
 }

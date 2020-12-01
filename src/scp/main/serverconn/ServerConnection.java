54db184847d305fb208618ffdc7ae8428e027d91
Zeale <zeale@dusttoash.org>
2020-12-01 10:12:22 -0600

    Update ServerConnection.java

--- src/scp/main/serverconn/ServerConnection.java
+++ src/scp/main/serverconn/ServerConnection.java
@@ -1,6 +1,7 @@
 package scp.main.serverconn;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 import scp.main.networkencoder.NetworkEncoder;
@@ -10,10 +11,30 @@ public class ServerConnection {
 
 	private Server server;
 
-	public void start(int port, Server server) {
-		// TODO Write the code to start up the ServerSocket that will listen for
-		// incoming clients. This will also need to setup the threads and other stuff
-		// that calls #receiveMessage(...) whenever a message is received from a client.
+	public void start(int port, Server server) throws Throwable {
+		@SuppressWarnings("resource") // 8)
+		ServerSocket serv = new ServerSocket(port);
+		Thread acceptor = new Thread(() -> {
+			while (true)
+				try {
+					Socket sock = serv.accept();
+					server.handleNewConnection(ServerConnection.this, sock);
+					Thread handler = new Thread(() -> {
+						while (true)
+							try {
+								receiveMessage(sock, NetworkEncoder.pollMessage(sock.getInputStream()));
+							} catch (Throwable e) {
+								throw new RuntimeException(e);
+							}
+					});
+					handler.setDaemon(true);
+					handler.start();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+		});
+		acceptor.setDaemon(true);
+		acceptor.start();
 	}
 
 	public void sendMessage(String message, Socket socket) throws Throwable {

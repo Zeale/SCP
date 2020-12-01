30e68e1d4d4653d76ec4ffa73f83321459ee9e9e
Zeale <zeale@dusttoash.org>
2020-12-01 10:50:26 -0600

    Update Server.java

--- src/scp/main/server/Server.java
+++ src/scp/main/server/Server.java
@@ -14,14 +14,13 @@ public class Server {
 	}
 
 	public void receiveMessage(String message, ServerConnection connection, Socket socket) throws Throwable {
-		String msg = users.get(socket).getUsername() + ": " + message;
+		String msg = "[#FF0000]" + users.get(socket).getUsername() + ": [#FFFFFF]" + message;
 		for (Socket s : users.keySet())
 			if (s != socket)
 				s.getOutputStream().write(NetworkEncoder.encodeMessage(msg));
 	}
 
 	public void handleNewConnection(ServerConnection connection, Socket newConnection) throws Throwable {
-		users.put(newConnection,
-				new User(newConnection, NetworkEncoder.pollMessage(newConnection.getInputStream()).substring(7)));
+		users.put(newConnection, new User(newConnection, NetworkEncoder.pollMessage(newConnection.getInputStream())));
 	}
 }

b3221b261623bb2f15a78f5fc778fa8e95771e04
Zeale <zeale@dusttoash.org>
2020-11-30 21:27:23 -0600

    Create ColorParser.java

---
+++ src/scp/main/colorencoder/ColorParser.java
@@ -0,0 +1,14 @@
+package scp.main.colorencoder;
+
+import java.util.List;
+
+import javafx.scene.text.Text;
+
+public class ColorParser {
+	
+	private CurrentFormatting curr;
+	
+	public final List<Text> parseFormatting(String text) {
+		
+	}
+}

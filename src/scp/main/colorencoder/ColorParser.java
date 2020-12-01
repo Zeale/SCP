065938843372586a2c798890afb0319de1dd73f2
Zeale <zeale@dusttoash.org>
2020-12-01 10:50:22 -0600

    Update ColorParser.java

--- src/scp/main/colorencoder/ColorParser.java
+++ src/scp/main/colorencoder/ColorParser.java
@@ -3,12 +3,13 @@ package scp.main.colorencoder;
 import java.util.List;
 
 import javafx.scene.text.Text;
+import zeale.apps.tools.console.std.ConsoleItem;
 
 public class ColorParser {
-	
+
 	private CurrentFormatting curr;
-	
-	public final List<Text> parseFormatting(String text) {
-		
+
+	public final List<ConsoleItem> parseFormatting(String text) {
+
 	}
 }

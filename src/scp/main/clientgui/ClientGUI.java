96b2c8ddd779e08c930b2b39a7c84a2c08d95260
Zeale <zeale@dusttoash.org>
2020-12-01 10:50:19 -0600

    Update ClientGUI.java

--- src/scp/main/clientgui/ClientGUI.java
+++ src/scp/main/clientgui/ClientGUI.java
@@ -8,9 +8,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import scp.main.clientconn.ClientConnection;
+import scp.main.colorencoder.ColorParser;
+import scp.main.server.Server;
+import scp.main.serverconn.ServerConnection;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
+import zeale.apps.tools.console.std.ConsoleItem;
+import zeale.apps.tools.console.std.StandardConsole;
 
 public class ClientGUI extends Window {
 
@@ -19,17 +27,45 @@ public class ClientGUI extends Window {
 	 */
 
 	private @FXML TextField serverIP, serverPort, username, localPort;
+	private ApplicationProperties props;
 
 	private @FXML void launchClient() {
-		// TODO Auto-generated method stub
+		String username = this.username.getText();
+		ClientConnection conn = new ClientConnection();
+		conn.connect(serverIP.getText(), Integer.parseInt(serverIP.getText()), username);
+		Stage stayj = new Stage();
+
+		StandardConsole consolee = new StandardConsole();
+		Scene seen = new Scene(new StackPane());
+		seen.getStylesheets().setAll(props.popButtonStylesheet.get(), props.themeStylesheet.get());
+		stayj.setScene(seen);
+		stayj.setTitle("Client: [" + serverIP.getText() + ':' + serverPort.getText() + ']');
+		stayj.show();
+		consolee.applyLogic(input -> {
+			String txt = input.text.trim();
+			consolee.println('[' + username + "]: " + txt, Color.ORANGE);
+			if (!txt.isEmpty())
+				conn.sendMessage(txt);
+		});
+		conn.registerConsumer(t -> {
+			ColorParser cp = new ColorParser();
+			Platform.runLater(() -> {
+				for (ConsoleItem ci : cp.parseFormatting(t))
+					consolee.write(ci);
+			});
+		});
 	}
 
 	private @FXML void launchServer() {
-		// TODO Auto-generated method stub
+		try {
+			Server server = new Server();
+			ServerConnection conn = new ServerConnection();
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	private @FXML void initialize() {
-		// TODO perform GUI initialization using FXML nodes above.
 	}
 
 	@Override
@@ -38,6 +74,7 @@ public class ClientGUI extends Window {
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		props = properties;
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("ClientGUI.fxml"));
 		loader.setController(this);
 		try {
